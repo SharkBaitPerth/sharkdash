67c1087f49c0882dbf89027ccd2e8b8fe0a965ee
Liam Jones <liam.jones.aus@gmail.com>
2016-07-31 07:03:07 +0000

    Add additional markers

--- static/map.js
+++ static/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-31.9505, 115.8605], 10);
+var map = L.map('map').setView([-31.9930, 115.7570], 11);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
   maxZoom: 18,
@@ -10,19 +10,33 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 
 L.AwesomeMarkers.Icon.prototype.options.prefix = 'fa';
 
+var dummyMarker = L.AwesomeMarkers.icon({
+  icon: 'check',
+  markerColor: 'green'
+});
+
 var greenMarker = L.AwesomeMarkers.icon({
-    icon: 'check',
-    markerColor: 'green'
-  });
+  icon: 'check',
+  markerColor: 'green',
+	extraClasses: 'icon-active'
+});
 
 var orangeMarker = L.AwesomeMarkers.icon({
-    icon: 'exclamation',
-    markerColor: 'orange'
-  });
+  icon: 'exclamation',
+  markerColor: 'orange',
+	extraClasses: 'icon-active'
+});
 
 var redMarker = L.AwesomeMarkers.icon({
-    icon: 'exclamation-triangle',
-    markerColor: 'red'
-  });
+  icon: 'exclamation-triangle',
+  markerColor: 'red',
+	extraClasses: 'icon-active'
+});
 
 var cottesloe = L.marker([-31.996, 115.7431], {icon: greenMarker}).addTo(map);
+
+$.getJSON("static/markers.json", function(json) {
+	json.markers.forEach(function(x) {
+		L.marker([x[0], x[1]], {icon: dummyMarker}).addTo(map);
+	});
+});
